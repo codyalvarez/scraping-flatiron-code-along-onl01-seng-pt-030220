0b9445c708838decf88a329bff74cdd0bcad2e65
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-12 02:11:07 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,17 @@
 class Course
   attr_accessor :title, :schedule, :descripion
+
+  @@all = []
+
+  def initialize 
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def self.reset_all 
+    @@all.clear
+  end
 end
