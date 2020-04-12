e742c622df46e022e6fa4fde78cfcd42caa2b0e6
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-12 02:11:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,15 +3,15 @@ class Course
 
   @@all = []
 
-  def initialize 
+  def initialize
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
-  def self.reset_all 
+  def self.reset_all
     @@all.clear
   end
 end
